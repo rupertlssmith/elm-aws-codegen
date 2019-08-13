84c30ecc544be4bb26c33a7f40243c0323bd5c2d
Rupert Smith <rupert@thesett.com>
2019-08-13 16:24:54 +0100

    Structural diffing.

--- src/elm/Diff.elm
+++ src/elm/Diff.elm
@@ -1,5 +1,6 @@
 module Diff exposing (..)
 
+import Dict
 import Json.Decode.Generic exposing (Json(..))
 
 
@@ -10,7 +11,7 @@ type alias Diff =
 diffsToString : List Diff -> String
 diffsToString diffs =
     List.map
-        (\( path, mismatch ) -> "")
+        (\( path, mismatch ) -> "At " ++ String.join " -> " path ++ " " ++ mismatch)
         diffs
         |> List.intersperse "\n"
         |> String.concat
@@ -18,7 +19,7 @@ diffsToString diffs =
 
 diff : Json -> Json -> List Diff
 diff left right =
-    diffInner left right []
+    diffInner left right [ "root" ]
 
 
 diffInner : Json -> Json -> List String -> List ( List String, String )
@@ -29,29 +30,65 @@ diffInner left right path =
                 []
 
             else
-                [ ( path, "string values" ) ]
+                [ ( List.reverse path, ": string values" ) ]
 
         ( JBool bvall, JBool bvalr ) ->
             if bvall == bvalr then
                 []
 
             else
-                [ ( path, "bool values" ) ]
+                [ ( List.reverse path, ": bool values" ) ]
 
         ( JInt ivall, JInt ivalr ) ->
-            []
+            if ivall == ivalr then
+                []
+
+            else
+                [ ( List.reverse path, ": int values" ) ]
 
         ( JFloat fvall, JFloat fvalr ) ->
-            []
+            if fvall == fvalr then
+                []
+
+            else
+                [ ( List.reverse path, ": float values" ) ]
 
         ( JNull, JNull ) ->
             []
 
         ( JObj dictl, JObj dictr ) ->
-            []
+            let
+                ddiff =
+                    Dict.diff dictl dictr
 
-        ( JArr listl, JArr listr ) ->
+                fieldDiffs =
+                    List.foldl
+                        (\key accum -> ( List.reverse path, ": key " ++ key ) :: accum)
+                        []
+                        (Dict.keys ddiff)
+
+                matchingFields =
+                    Dict.intersect dictl dictr
+                        |> Dict.keys
+
+                childDiffs =
+                    List.foldl
+                        (\key accum ->
+                            case ( Dict.get key dictl, Dict.get key dictr ) of
+                                ( Just fieldl, Just fieldr ) ->
+                                    diffInner fieldl fieldr (key :: path)
+                                        |> List.append accum
+
+                                ( _, _ ) ->
+                                    accum
+                        )
+                        []
+                        matchingFields
+            in
+            List.append fieldDiffs childDiffs
+
+        ( JArr arrl, JArr arrr ) ->
             []
 
         ( _, _ ) ->
-            []
+            [ ( List.reverse path, ": json types" ) ]
