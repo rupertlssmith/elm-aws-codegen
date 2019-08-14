6753cc88dda4b328e25322d8346276fc5e61ab0a
Rupert Smith <rupert@thesett.com>
2019-08-14 14:34:02 +0100

    Tidied up the AWS Service model and decoders.

--- src/elm/Service.elm
+++ src/elm/Service.elm
@@ -8,41 +8,41 @@ import Dict exposing (Dict)
 
 
 type alias Service =
-    { version : Maybe String
-    , metaData : MetaData
+    { metaData : MetaData
     , operations : Dict String Operation
     , shapes : Dict String Shape
-    , documentation : Maybe String
     , authorizers : Maybe Authorizers
+    , documentation : Maybe String
+    , version : Maybe String
     }
 
 
 serviceCodec =
     Codec.object Service
-        |> Codec.optionalField "version" .version Codec.string
         |> Codec.field "metadata" .metaData metaDataCodec
         |> Codec.field "operations" .operations (Codec.dict operationCodec)
         |> Codec.field "shapes" .shapes (Codec.dict shapeCodec)
-        |> Codec.optionalField "documentation" .documentation Codec.string
         |> Codec.optionalField "authorizers" .authorizers authorizersCodec
+        |> Codec.optionalField "documentation" .documentation Codec.string
+        |> Codec.optionalField "version" .version Codec.string
         |> Codec.buildObject
 
 
 type alias MetaData =
     { apiVersion : String
     , endpointPrefix : String
-    , jsonVersion : Maybe String
     , protocol : String
+    , serviceId : String
+    , checksumFormat : Maybe String
+    , globalEndpoint : Maybe String
+    , jsonVersion : Maybe String
     , serviceAbbreviation : Maybe String
     , serviceFullName : Maybe String
-    , serviceId : String
     , signatureVersion : Maybe String
+    , signingName : Maybe String
     , targetPrefix : Maybe String
     , uid : Maybe String
-    , signingName : Maybe String
-    , globalEndpoint : Maybe String
     , xmlNamespace : Maybe String
-    , checksumFormat : Maybe String
     }
 
 
@@ -50,18 +50,18 @@ metaDataCodec =
     Codec.object MetaData
         |> Codec.field "apiVersion" .apiVersion Codec.string
         |> Codec.field "endpointPrefix" .endpointPrefix Codec.string
-        |> Codec.optionalField "jsonVersion" .jsonVersion Codec.string
         |> Codec.field "protocol" .protocol Codec.string
+        |> Codec.field "serviceId" .serviceId Codec.string
+        |> Codec.optionalField "checksumFormat" .checksumFormat Codec.string
+        |> Codec.optionalField "globalEndpoint" .globalEndpoint Codec.string
+        |> Codec.optionalField "jsonVersion" .jsonVersion Codec.string
         |> Codec.optionalField "serviceAbbreviation" .serviceAbbreviation Codec.string
         |> Codec.optionalField "serviceFullName" .serviceFullName Codec.string
-        |> Codec.field "serviceId" .serviceId Codec.string
         |> Codec.optionalField "signatureVersion" .signatureVersion Codec.string
+        |> Codec.optionalField "signingName" .signingName Codec.string
         |> Codec.optionalField "targetPrefix" .targetPrefix Codec.string
         |> Codec.optionalField "uid" .uid Codec.string
-        |> Codec.optionalField "signingName" .signingName Codec.string
-        |> Codec.optionalField "globalEndpoint" .globalEndpoint Codec.string
         |> Codec.optionalField "xmlNamespace" .xmlNamespace Codec.string
-        |> Codec.optionalField "checksumFormat" .checksumFormat Codec.string
         |> Codec.buildObject
 
 
@@ -78,16 +78,16 @@ authorizersCodec =
 
 type alias AuthorizationStrategy =
     { name : String
-    , type_ : String
     , placement : Placement
+    , type_ : String
     }
 
 
 authorizationStrategyCodec =
     Codec.object AuthorizationStrategy
         |> Codec.field "name" .name Codec.string
-        |> Codec.field "type" .type_ Codec.string
         |> Codec.field "placement" .placement placementCodec
+        |> Codec.field "type" .type_ Codec.string
         |> Codec.buildObject
 
 
@@ -105,37 +105,37 @@ placementCodec =
 
 
 type alias Operation =
-    { name : String
-    , http : Http
-    , input : Maybe ShapeRef
-    , output : Maybe ShapeRef
-    , errors : Maybe (List ShapeRef)
-    , idempotent : Maybe Bool
-    , endpoint : Maybe Endpoint
-    , documentation : Maybe String
-    , deprecated : Maybe Bool
-    , deprecatedMessage : Maybe String
-    , documentationUrl : Maybe String
+    { http : Http
+    , name : String
     , alias : Maybe String
     , authtype : Maybe String
+    , deprecated : Maybe Bool
+    , deprecatedMessage : Maybe String
+    , documentation : Maybe String
+    , documentationUrl : Maybe String
+    , endpoint : Maybe Endpoint
+    , errors : Maybe (List ShapeRef)
+    , idempotent : Maybe Bool
+    , input : Maybe ShapeRef
+    , output : Maybe ShapeRef
     }
 
 
 operationCodec =
     Codec.object Operation
-        |> Codec.field "name" .name Codec.string
         |> Codec.field "http" .http httpCodec
-        |> Codec.optionalField "input" .input shapeRefCodec
-        |> Codec.optionalField "output" .output shapeRefCodec
-        |> Codec.optionalField "errors" .errors (Codec.list shapeRefCodec)
-        |> Codec.optionalField "idempotent" .idempotent Codec.bool
-        |> Codec.optionalField "endpoint" .endpoint endpointCodec
-        |> Codec.optionalField "documentation" .documentation Codec.string
-        |> Codec.optionalField "deprecated" .deprecated Codec.bool
-        |> Codec.optionalField "deprecatedMessage" .deprecatedMessage Codec.string
-        |> Codec.optionalField "documentationUrl" .documentationUrl Codec.string
+        |> Codec.field "name" .name Codec.string
         |> Codec.optionalField "alias" .alias Codec.string
         |> Codec.optionalField "authtype" .authtype Codec.string
+        |> Codec.optionalField "deprecated" .deprecated Codec.bool
+        |> Codec.optionalField "deprecatedMessage" .deprecatedMessage Codec.string
+        |> Codec.optionalField "documentation" .documentation Codec.string
+        |> Codec.optionalField "documentationUrl" .documentationUrl Codec.string
+        |> Codec.optionalField "endpoint" .endpoint endpointCodec
+        |> Codec.optionalField "errors" .errors (Codec.list shapeRefCodec)
+        |> Codec.optionalField "idempotent" .idempotent Codec.bool
+        |> Codec.optionalField "input" .input shapeRefCodec
+        |> Codec.optionalField "output" .output shapeRefCodec
         |> Codec.buildObject
 
 
@@ -153,8 +153,8 @@ endpointCodec =
 type alias Http =
     { method : String
     , requestUri : Maybe String
-    , responseCode : Maybe Int
     , requireUri : Maybe Bool
+    , responseCode : Maybe Int
     }
 
 
@@ -162,8 +162,8 @@ httpCodec =
     Codec.object Http
         |> Codec.field "method" .method Codec.string
         |> Codec.optionalField "requestUri" .requestUri Codec.string
-        |> Codec.optionalField "responseCode" .responseCode Codec.int
         |> Codec.optionalField "requireUri" .requireUri Codec.bool
+        |> Codec.optionalField "responseCode" .responseCode Codec.int
         |> Codec.buildObject
 
 
@@ -184,75 +184,75 @@ errorCodec =
 
 type alias ShapeRef =
     { shape : String
+    , box : Maybe Bool
+    , deprecated : Maybe Bool
+    , deprecatedMessage : Maybe String
     , documentation : Maybe String
+    , eventpayload : Maybe Bool
+    , flattened : Maybe Bool
     , idempotencyToken : Maybe String
+    , jsonvalue : Maybe Bool
     , location : Maybe String
     , locationName : Maybe String
-    , deprecated : Maybe Bool
-    , box : Maybe Bool
-    , resultWrapper : Maybe String
-    , flattened : Maybe Bool
-    , deprecatedMessage : Maybe String
-    , xmlNamespace : Maybe String
-    , xmlAttribute : Maybe String
-    , streaming : Maybe Bool
-    , eventpayload : Maybe Bool
-    , jsonvalue : Maybe Bool
     , queryName : Maybe String
+    , resultWrapper : Maybe String
+    , streaming : Maybe Bool
     , timestampFormat : Maybe String
+    , xmlAttribute : Maybe String
+    , xmlNamespace : Maybe String
     }
 
 
 shapeRefCodec =
     Codec.object ShapeRef
         |> Codec.field "shape" .shape Codec.string
+        |> Codec.optionalField "box" .box Codec.bool
+        |> Codec.optionalField "deprecated" .deprecated Codec.bool
+        |> Codec.optionalField "deprecatedMessage" .deprecatedMessage Codec.string
         |> Codec.optionalField "documentation" .documentation Codec.string
+        |> Codec.optionalField "eventpayload" .eventpayload Codec.bool
+        |> Codec.optionalField "flattened" .flattened Codec.bool
         |> Codec.optionalField "idempotencyToken" .idempotencyToken Codec.string
+        |> Codec.optionalField "jsonvalue" .jsonvalue Codec.bool
         |> Codec.optionalField "location" .location Codec.string
         |> Codec.optionalField "locationName" .locationName Codec.string
-        |> Codec.optionalField "deprecated" .deprecated Codec.bool
-        |> Codec.optionalField "box" .box Codec.bool
-        |> Codec.optionalField "resultWrapper" .resultWrapper Codec.string
-        |> Codec.optionalField "flattened" .flattened Codec.bool
-        |> Codec.optionalField "deprecatedMessage" .deprecatedMessage Codec.string
-        |> Codec.optionalField "xmlNamespace" .xmlNamespace Codec.string
-        |> Codec.optionalField "xmlAttribute" .xmlAttribute Codec.string
-        |> Codec.optionalField "streaming" .streaming Codec.bool
-        |> Codec.optionalField "eventpayload" .eventpayload Codec.bool
-        |> Codec.optionalField "jsonvalue" .jsonvalue Codec.bool
         |> Codec.optionalField "queryName" .queryName Codec.string
+        |> Codec.optionalField "resultWrapper" .resultWrapper Codec.string
+        |> Codec.optionalField "streaming" .streaming Codec.bool
         |> Codec.optionalField "timestampFormat" .timestampFormat Codec.string
+        |> Codec.optionalField "xmlAttribute" .xmlAttribute Codec.string
+        |> Codec.optionalField "xmlNamespace" .xmlNamespace Codec.string
         |> Codec.buildObject
 
 
 type alias Shape =
-    { type_ : Types
-    , required : Maybe (List String)
-    , max : Maybe Int
-    , min : Maybe Int
-    , pattern : Maybe String
-    , members : Maybe (Dict String ShapeRef)
-    , member : Maybe ShapeRef
-    , key : Maybe ShapeRef
-    , value : Maybe ShapeRef
-    , enum : Maybe (List String)
-    , sensitive : Maybe Bool
+    { type_ : AWSType
+    , box : Maybe Bool
     , deprecated : Maybe Bool
     , deprecatedMessage : Maybe String
-    , box : Maybe Bool
+    , documentation : Maybe String
+    , enum : Maybe (List String)
+    , error : Maybe Error
+    , event : Maybe Bool
+    , eventstream : Maybe Bool
     , exception : Maybe Bool
     , fault : Maybe Bool
     , flattened : Maybe Bool
-    , documentation : Maybe String
+    , key : Maybe ShapeRef
     , locationName : Maybe String
-    , timestampFormat : Maybe String
+    , max : Maybe Int
+    , member : Maybe ShapeRef
+    , members : Maybe (Dict String ShapeRef)
+    , min : Maybe Int
+    , pattern : Maybe String
     , payload : Maybe String
-    , wrapper : Maybe Bool
-    , event : Maybe Bool
-    , xmlNamespace : Maybe String
-    , error : Maybe Error
-    , eventstream : Maybe Bool
+    , required : Maybe (List String)
+    , sensitive : Maybe Bool
     , streaming : Maybe Bool
+    , timestampFormat : Maybe String
+    , value : Maybe ShapeRef
+    , wrapper : Maybe Bool
+    , xmlNamespace : Maybe String
     , xmlOrder : Maybe (List String)
     }
 
@@ -260,132 +260,128 @@ type alias Shape =
 shapeCodec =
     Codec.object Shape
         |> Codec.field "type" .type_ typesCodec
-        |> Codec.optionalField "required" .required (Codec.list Codec.string)
-        |> Codec.optionalField "max" .max Codec.int
-        |> Codec.optionalField "min" .min Codec.int
-        |> Codec.optionalField "pattern" .pattern Codec.string
-        |> Codec.optionalField "members" .members (Codec.dict shapeRefCodec)
-        |> Codec.optionalField "member" .member shapeRefCodec
-        |> Codec.optionalField "key" .key shapeRefCodec
-        |> Codec.optionalField "value" .value shapeRefCodec
-        |> Codec.optionalField "enum" .enum (Codec.list Codec.string)
-        |> Codec.optionalField "sensitive" .sensitive Codec.bool
+        |> Codec.optionalField "box" .box Codec.bool
         |> Codec.optionalField "deprecated" .deprecated Codec.bool
         |> Codec.optionalField "deprecatedMessage" .deprecatedMessage Codec.string
-        |> Codec.optionalField "box" .box Codec.bool
+        |> Codec.optionalField "documentation" .documentation Codec.string
+        |> Codec.optionalField "enum" .enum (Codec.list Codec.string)
+        |> Codec.optionalField "error" .error errorCodec
+        |> Codec.optionalField "event" .event Codec.bool
+        |> Codec.optionalField "eventstream" .eventstream Codec.bool
         |> Codec.optionalField "exception" .exception Codec.bool
         |> Codec.optionalField "fault" .fault Codec.bool
         |> Codec.optionalField "flattened" .flattened Codec.bool
-        |> Codec.optionalField "documentation" .documentation Codec.string
+        |> Codec.optionalField "key" .key shapeRefCodec
         |> Codec.optionalField "locationName" .locationName Codec.string
-        |> Codec.optionalField "timestampFormat" .timestampFormat Codec.string
+        |> Codec.optionalField "max" .max Codec.int
+        |> Codec.optionalField "member" .member shapeRefCodec
+        |> Codec.optionalField "members" .members (Codec.dict shapeRefCodec)
+        |> Codec.optionalField "min" .min Codec.int
+        |> Codec.optionalField "pattern" .pattern Codec.string
         |> Codec.optionalField "payload" .payload Codec.string
-        |> Codec.optionalField "wrapper" .wrapper Codec.bool
-        |> Codec.optionalField "event" .event Codec.bool
-        |> Codec.optionalField "xmlNamespace" .xmlNamespace Codec.string
-        |> Codec.optionalField "error" .error errorCodec
-        |> Codec.optionalField "eventstream" .eventstream Codec.bool
+        |> Codec.optionalField "required" .required (Codec.list Codec.string)
+        |> Codec.optionalField "sensitive" .sensitive Codec.bool
         |> Codec.optionalField "streaming" .streaming Codec.bool
+        |> Codec.optionalField "timestampFormat" .timestampFormat Codec.string
+        |> Codec.optionalField "value" .value shapeRefCodec
+        |> Codec.optionalField "wrapper" .wrapper Codec.bool
+        |> Codec.optionalField "xmlNamespace" .xmlNamespace Codec.string
         |> Codec.optionalField "xmlOrder" .xmlOrder (Codec.list Codec.string)
         |> Codec.buildObject
 
 
-type Types
-    = String -- Enum
-    | Boolean
-    | Integer
-    | Long
-    | Float
-    | Double
-    | Blob
-    | Structure
-    | List
-    | Map
-    | Timestamp
-    | Unknown
+type AWSType
+    = AString
+    | ABoolean
+    | AInteger
+    | ALong
+    | AFloat
+    | ADouble
+    | ABlob
+    | AStructure
+    | AList
+    | AMap
+    | ATimestamp
+    | AUnknown
 
 
-typesCodec : Codec Types
+typesCodec : Codec AWSType
 typesCodec =
     Codec.map
         (\val ->
             case val of
                 "string" ->
-                    String
+                    AString
 
                 "boolean" ->
-                    Boolean
+                    ABoolean
 
                 "integer" ->
-                    Integer
+                    AInteger
 
                 "long" ->
-                    Long
+                    ALong
 
                 "float" ->
-                    Float
+                    AFloat
 
                 "double" ->
-                    Double
+                    ADouble
 
                 "blob" ->
-                    Blob
+                    ABlob
 
                 "structure" ->
-                    Structure
+                    AStructure
 
                 "list" ->
-                    List
+                    AList
 
                 "map" ->
-                    Map
+                    AMap
 
                 "timestamp" ->
-                    Timestamp
+                    ATimestamp
 
                 str ->
-                    let
-                        _ =
-                            Debug.log "unknown" str
-                    in
-                    Unknown
+                    AUnknown
         )
         (\types ->
             case types of
-                String ->
+                AString ->
                     "string"
 
-                Boolean ->
+                ABoolean ->
                     "boolean"
 
-                Integer ->
+                AInteger ->
                     "integer"
 
-                Long ->
+                ALong ->
                     "long"
 
-                Float ->
+                AFloat ->
                     "float"
 
-                Double ->
+                ADouble ->
                     "double"
 
-                Blob ->
+                ABlob ->
                     "blob"
 
-                Structure ->
+                AStructure ->
                     "structure"
 
-                List ->
+                AList ->
                     "list"
 
-                Map ->
+                AMap ->
                     "map"
 
-                Timestamp ->
+                ATimestamp ->
                     "timestamp"
 
-                Unknown ->
+                AUnknown ->
                     "unknown"
         )
         Codec.string
