288cf30f65288eb3455f1f4841d66c2e72d02c61
Rupert Smith <rupert@thesett.com>
2019-10-04 14:28:11 +0100

    Writing some docs for L1.

--- src/elm/L1.elm
+++ src/elm/L1.elm
@@ -7,6 +7,18 @@ module L1 exposing
     , Type(..)
     )
 
+{-| L1 is a data modelling language.
+
+The name L1 stands for 'level 1', as it is envisioned to be part of a sequence of
+similar data modelling languages, 'level 2', 'level 3'. With L1 being completely
+language agnostic, and higher levels bringing in more specific information targetting
+code generation on particular languages or systems.
+
+What you see here is a first pass at describing the L1 modelling language, and using
+it in a practical context of generating AWS Service stubs for Elm.
+
+-}
+
 import Dict exposing (Dict)
 
 
