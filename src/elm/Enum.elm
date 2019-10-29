35fff0a47e01caaf9c5f87968edda94322e8c40f
Rupert Smith <rupert@thesett.com>
2019-10-29 16:41:53 +0000

    Defined support module for enums, will get extracted to its own package.

---
+++ src/elm/Enum.elm
@@ -0,0 +1,40 @@
+module Enum exposing (..)
+
+import Json.Decode as Decode exposing (Decoder)
+import Json.Encode as Encode exposing (Value)
+
+
+type Enum a
+    = Enum (List a) (a -> String)
+
+
+make : List a -> (a -> String) -> Enum a
+make vals toString =
+    Enum vals toString
+
+
+find : Enum a -> String -> Maybe a
+find (Enum vals toString) val =
+    vals
+        |> List.filter ((==) val << toString)
+        |> List.head
+
+
+decoder : Enum a -> Decoder a
+decoder enum =
+    Decode.string
+        |> Decode.andThen
+            (\val ->
+                case find enum val of
+                    Just value ->
+                        Decode.succeed value
+
+                    Nothing ->
+                        Decode.fail <| "Could not decode value to enum: " ++ val
+            )
+
+
+encoder : Enum a -> a -> Value
+encoder (Enum _ toString) val =
+    toString val
+        |> Encode.string
