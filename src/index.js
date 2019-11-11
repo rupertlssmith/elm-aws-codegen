fe97fa47fc468c6d4cefcac3df6ec1e930ce5e97
Rupert Smith <rupert@thesett.com>
2019-11-11 15:58:31 +0000

    Processing multiple files.

--- src/index.js
+++ src/index.js
@@ -19,9 +19,23 @@ const app = Elm.Top.init();
 // fs.readFile('api/dynamodb-2012-08-10.normal.json', 'utf8', function(err, contents) {
 //   app.ports.modelInPort.send(['api/dynamodb-2012-08-10.normal.json', contents]);
 // });
+// fs.readFile('api/athena-2017-05-18.normal.json', 'utf8', function(err, contents) {
+//   app.ports.modelInPort.send(['api/athena-2017-05-18.normal.json', contents]);
+// });
 
-fs.readFile('api/athena-2017-05-18.normal.json', 'utf8', function(err, contents) {
-  app.ports.modelInPort.send(['api/athena-2017-05-18.normal.json', contents]);
+const specs = [
+  "athena-2017-05-18.normal.json",
+  "dynamodb-2012-08-10.normal.json"
+]
+
+specs.forEach(function(item, index) {
+  var filename = 'api/' + item;
+
+  console.log(filename);
+
+  fs.readFile(filename, 'utf8', function(err, contents) {
+    app.ports.modelInPort.send([filename, contents]);
+  });
 });
 
 app.ports.codeOutPort.subscribe(request => {
