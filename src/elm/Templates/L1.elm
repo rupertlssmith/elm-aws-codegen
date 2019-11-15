bb1bae5a619729e47217646795d9ce2bfdfe3503
Rupert Smith <rupert@thesett.com>
2019-11-15 16:22:23 +0000

    Corrected parens on chain of Results, but fix should be pushed down into the mChain function.

--- src/elm/Templates/L1.elm
+++ src/elm/Templates/L1.elm
@@ -201,8 +201,8 @@ restrictedString name res =
                 guardFn =
                     CG.opApply "|>"
                         CG.infixLeft
-                        (Util.mChainResult (CG.apply [ gd, CG.val "val" ])
-                            (List.map CG.parens gds)
+                        (CG.pipe (CG.apply [ gd, CG.val "val" ])
+                            (List.map (\expr -> CG.apply [ CG.fqFun resultMod "andThen", CG.parens expr ]) gds)
                         )
                         typeWrapper
                         |> CG.letFunction "guardFn" [ CG.varPattern "val" ]
