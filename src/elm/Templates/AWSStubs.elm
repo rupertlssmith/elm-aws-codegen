c1cfc9b439537be1537e7ca6e84febfb812d61b8
rupertlssmith <github@badgerhouse.freeserve.co.uk>
2020-02-18 21:48:23 +0000

    Defining the defaults for the AWS stub generation.

--- src/elm/Templates/AWSStubs.elm
+++ src/elm/Templates/AWSStubs.elm
@@ -1,16 +1,43 @@
-module Templates.AWSStubs exposing (..)
+module Templates.AWSStubs exposing (AWSStubsError(..), check, defaultProperties, errorToString, generate, generator)
 
 import Dict
 import Elm.CodeGen as CG exposing (File)
+import L1 exposing (Properties, Property(..))
 import L2 exposing (L2)
-import L3 exposing (Processor)
+import L3 exposing (DefaultProperties, Processor)
 import ResultME exposing (ResultME)
+
+
+defaultProperties : DefaultProperties
+defaultProperties =
+    { top =
+        Dict.fromList
+            [ ( "name", PQName [ "AWS" ] "Stub" )
+            , ( "isRegional", PBool False )
+            , ( "apiVersion", PString "1.0" )
+            , ( "protocol", PEnum "JSON" )
+            , ( "signer", PEnum "V4" )
+            , ( "xmlNamespace", PString "" )
+            , ( "targetPrefix", PString "" )
+            , ( "signingName", PString "" )
+            , ( "jsonVersion", PString "" )
+            , ( "documentation", PString "" )
+            ]
+    , alias =
+        Dict.fromList
+            [ ( "awsStubGeneration", POptional Nothing ) ]
+    , sum = Dict.empty
+    , enum =
+        Dict.fromList
+            [ ( "elmEnumStyle", PEnum "customType" ) ]
+    , fields = Dict.empty
+    }
 
 
 generator : Processor pos AWSStubsError
 generator =
     { name = "AWSStubs"
-    , defaults = Dict.empty
+    , defaults = defaultProperties
     , check = check
     , errorToString = errorToString
     }
